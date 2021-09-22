c7d81885ab753eb8556813f853ba4fa4300aa97a
Leonardo Ascenzi <lascenzi@colgate.edu>
2021-09-22 00:12:46 -0400

    feat(*): Added tyler's todos

--- src/common/SignUp/index.js
+++ src/common/SignUp/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import styles from './styles';
 import { withStyles } from '@mui/styles';
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 
 const SignUp = ({ classes }) => {
+    const registerUser = () => {
+        // TODO save users email in firestore
+    };
     return (
         <div className={classes.root}>
             <Typography>Sign Up Below</Typography>
             {/* TODO add sign up form here @Tyler */}
+            <Button onClick={registerUser}>Dummy Button</Button>
         </div>
     );
 };
